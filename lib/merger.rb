82310cdef82e5605ce6111fb595e77780ac56259
danm <writetodan@yahoo.com>
2015-07-05 21:00:40 +0100

    @damned constructed card from story; add if not on wall, leave if already there; identify by 'mingle' source and mingle-specific id; 'alert' field (set when new) will make wall draw attention to card, with a message

--- lib/merger.rb
+++ lib/merger.rb
@@ -1,9 +1,48 @@
 class Merger
-  def merge(mingle_cards, wall_cards)
-    mingle_cards.map {|card|
-      {
-          text: "##{card['Number']}\n#{card['Name']}"
-      }
+  def initialize(mingle)
+    @mingle = mingle
+  end
+
+  def merge(mingle_stories, wall_cards)
+    merged = wall_cards.clone
+    mingle_stories.each {|story|
+      unless wall_cards.any? {|card| card_of_story?(card, story) }
+        merged << card_from_story(story)
+      end
     }
+    merged
+  end
+
+  private
+
+  def card_of_story?(card, story)
+    card[:source] == 'mingle' && card[:id] == card_uri(story)
+  end
+
+  def card_from_story(story)
+    {
+        source: 'mingle',
+        id: card_uri(story),
+        top: 0,
+        left: 0,
+        text: card_text(story),
+        alert: 'New card'
+    }
+  end
+
+  def card_text(story)
+    "##{story['Number']}\n#{story['Name']}"
+  end
+
+  def card_uri(story)
+    "mingle://#{hostname}/#{project}##{story['Number']}"
+  end
+
+  def hostname
+    @mingle.hostname
+  end
+
+  def project
+    @mingle.project
   end
 end
--- spec/merger_spec.rb
+++ spec/merger_spec.rb
@@ -2,23 +2,66 @@ require 'rspec'
 require_relative '../lib/merger'
 
 describe Merger do
-  let(:wall_cards) { [] }
-  let(:mingle_cards) { [] }
+  let(:stories) { [] }
+  let(:mingle) { double('mingle', hostname: 'mingle.tw.com', project: 'myproject') }
 
-  let(:merger) { Merger.new }
-  subject(:cards) { Merger.new.merge(mingle_cards, wall_cards) }
+  let(:merger) { Merger.new(mingle) }
+  subject(:merged) { merger.merge(stories, wall_cards) }
 
   context 'a mingle card' do
-    let(:mingle_cards) { [a_story] }
+    let(:stories) { [ story ] }
+    let(:story) { a_story('Number' => '123') }
 
-    it 'is added' do
-      expect(cards.size).to eq 1
-      expect(cards.first[:text]).to start_with '#' + a_story['Number']
-      expect(cards.first[:text]).to end_with a_story['Name']
+    context 'it not on wall' do
+      let(:wall_cards) { [] }
+
+      it 'is added' do
+        expect(merged.size).to eq 1
+        expect(merged.first[:text]).to start_with '#123'
+        expect(merged.first[:text]).to end_with story['Name']
+      end
+
+      it 'has identifying source and id set' do
+        expect(merged.first[:source]).to eq 'mingle'
+        expect(merged.first[:id]).to eq 'mingle://mingle.tw.com/myproject#123'
+      end
+
+      it 'is placed at top left' do
+        expect(merged.first[:left]).to eq 0
+        expect(merged.first[:top]).to eq 0
+      end
+
+      it 'will be marked with an alert' do
+        expect(merged.first[:alert]).to eq 'New card'
+      end
     end
+
+    context 'it is already on wall' do
+      let(:existing_card) do
+        {
+          source: 'mingle',
+          id: 'mingle://mingle.tw.com/myproject#123',
+          text: '#123 The story',
+          top: 100,
+          left: 50
+        }
+      end
+      let(:wall_cards) { [ existing_card ] }
+
+      it 'is left on wall' do
+        expect(merged.size).to eq 1
+        expect(merged.first[:id]).to eq existing_card[:id]
+      end
+
+      it 'is left at current location' do
+        expect(merged.first[:top]).to eq existing_card[:top]
+        expect(merged.first[:left]).to eq existing_card[:left]
+      end
+    end
+
   end
 
-  def a_story
-    {'Name' => 'a mingle story', 'Number' => '123', 'Status' => 'Next'}
+  def a_story(overrides = {})
+    {'Name' => 'a mingle story', 'Number' => '123', 'Status' => 'Next'}.merge overrides
   end
 end
